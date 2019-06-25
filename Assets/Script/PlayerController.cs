b3532e803b329cff800127bb59b2a03b64ce8ca1
rbwebdev <brasier.romain@gmail.com>
2019-06-25 22:21:45 +0200

    sandbox tic tac toe

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Transform prefabX;
+    public Transform prefabO;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        if (Input.GetKeyDown("x"))
+        {
+            Instantiate(prefabX, transform.position, transform.rotation);
+        }
+
+        if (Input.GetKeyDown("o"))
+        {
+            Instantiate(prefabO, transform.position, transform.rotation);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
