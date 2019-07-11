ae985e2ae7027cd28837602b0f46cfe1debcb5c5
Jonahtan Jorand <jonathan.jorand@gmail.com>
2019-07-11 13:26:16 +0200

    worms setup

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob : MonoBehaviour
+{
+    public float hearthPoint;
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("ici");
+    }
+
+    protected Transform GetTransformPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
+
+        return player.transform;
+    }
+
+    protected void DoDamage()
+    {
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hearthPoint -= damage;
+        if (hearthPoint < 0)
+        {
+            Debug.Log("dead");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Worm.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worm : Mob
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform player = GetTransformPlayer();
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+}
