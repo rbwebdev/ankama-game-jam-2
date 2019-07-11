38013807b6c7541ebe6f90d1583cee0fb1436f6a
Jonahtan Jorand <jonathan.jorand@gmail.com>
2019-07-11 18:33:35 +0200

    wasp

---
+++ Assets/Scripts/Wasp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wasp : Mob
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dead)
+        {
+            Transform player = GetTransformPlayer();
+            Debug.Log(player.position.x < transform.position.x);
+            if (player.position.x < transform.position.x)
+            {
+                transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
+            } else
+            {
+                transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+            }
+        }
+    }
+}
