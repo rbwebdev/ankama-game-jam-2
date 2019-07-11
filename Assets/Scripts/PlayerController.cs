20ab917a4e9799150cf9225b56c1cd4178dd83d8
rbwebdev <brasier.romain@gmail.com>
2019-07-11 12:02:57 +0200

    romain sandbox

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public float jumpPower;
+    public bool isGrounded;
+
+    float targetMoveSpeed;
+
+    private void Update()
+    {
+        targetMoveSpeed = Mathf.Lerp(rb.velocity.x, Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, Time.deltaTime * 10);
+        rb.velocity = new Vector2(targetMoveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+}
