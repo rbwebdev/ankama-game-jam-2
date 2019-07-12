79ebca4103b3094029dff2d9359dacd3ee26f1dc
rbwebdev <brasier.romain@gmail.com>
2019-07-12 11:08:11 +0200

    UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,13 +10,20 @@ public class PlayerController : MonoBehaviour
     public float jumpPower;
     public bool isGrounded;
     public GameObject weaponSprite;
+    public TMP_Text textHP;
+    public TMP_Text textAmmo;
+    public TMP_Text textDamages;
+    public TMP_Text textPoints;
+    public GameObject DPSBoostedSprite;
+    public GameObject DamagesBoostedSprite;
 
     public float healthPoints;
-
-    //private bool isLeft;
-    //private bool isRight;
-
+    
     private float healthPointsForReset;
+
+    private bool dspBoosted = false;
+    private bool damagesBoosted = false;
+    private int points = 0;
 
     float targetMoveSpeed;
     private Vector2 mouse;
@@ -24,6 +32,10 @@ public class PlayerController : MonoBehaviour
     {
         isGrounded = true;
         healthPointsForReset = healthPoints;
+        printHP();
+        printAmmo();
+        printDamages();
+        printPoints();
     }
 
     private void Update()
@@ -59,59 +71,130 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.tag == "Medic")
         {
-            float boostPoints = collision.gameObject.GetComponent<Medic>().healValue;
-            float healthPointsForReset = healthPoints;
-            if (healthPoints + boostPoints > healthPoints)
+            float healValue = collision.gameObject.GetComponent<Medic>().healValue;
+            float tmpHeal = healthPoints + healValue;
+            if (tmpHeal < healthPointsForReset)
             {
-                healthPoints = healthPointsForReset;
+                healthPoints = healthPoints + healValue;
             }
             else
             {
-                healthPoints += boostPoints;
+                healthPoints = healthPointsForReset;
             }
+            printHP();
             Destroy(collision.gameObject);
         }
         if (collision.gameObject.tag == "BoostDamages")
         {
             float boostPoints = collision.gameObject.GetComponent<BoostDamages>().boostPoints;
             float boostTime = collision.gameObject.GetComponent<BoostDamages>().boostTime;
-            StartCoroutine(BoostDamages(boostPoints, boostTime));
             Destroy(collision.gameObject);
+            if (!damagesBoosted)
+            {
+                damagesBoosted = true;
+                StartCoroutine(BoostDamages(boostPoints, boostTime));
+            }
         }
         if (collision.gameObject.tag == "BoostDPS")
         {
             float boostMultiplicator = collision.gameObject.GetComponent<BoostDPS>().boostMultiplicator;
             float boostTime = collision.gameObject.GetComponent<BoostDPS>().boostTime;
             Destroy(collision.gameObject);
-            StartCoroutine(BoostDPS(boostMultiplicator, boostTime));
+            if (!dspBoosted)
+            {
+                dspBoosted = true;
+                StartCoroutine(BoostDPS(boostMultiplicator, boostTime));
+            }
         }
     }
 
     public void TakeDamage(float damage)
     {
         healthPoints -= damage;
+        printHP();
         if (healthPoints <= 0)
         {
             GameOver();
         }
     }
 
-   void GameOver()
+    public void TakePoints(int takePoints)
+    {
+        points += takePoints;
+        printPoints();
+    }
+
+    void GameOver()
    {
         Destroy(gameObject);
    }
 
+   private void printHP()
+   {
+        textHP.text = "HP: " + healthPoints.ToString();
+   }
+
+   private void printAmmo()
+   {
+        float tmpStartTimeBtwShots = GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().startTimeBtwShots;
+        float tmpAmmo = 1 / tmpStartTimeBtwShots;
+        textAmmo.text = "Ammo: " + tmpAmmo + "/s";
+   }
+
+   private void printDamages()
+   {
+        textDamages.text = "Damages: " + GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().damage + "/hit";
+   }
+
+   private void printPoints()
+   {
+        textPoints.text = "Points: " + points.ToString();
+   }
+
     IEnumerator BoostDamages(float boostPoints, float boostTime)
    {
-        GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().damage += boostPoints;
+        GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().damage = GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().damage + boostPoints;
+        printDamages();
+        if (dspBoosted)
+        {
+            DamagesBoostedSprite.GetComponent<RectTransform>().position = new Vector2(135, DamagesBoostedSprite.GetComponent<RectTransform>().position.y);
+        }
+        else
+        {
+            DamagesBoostedSprite.GetComponent<RectTransform>().position = new Vector2(110, DamagesBoostedSprite.GetComponent<RectTransform>().position.y);
+        }
+        DamagesBoostedSprite.SetActive(true);
         yield return new WaitForSeconds(boostTime);
-        GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().damage -= boostPoints;
-   }
+        GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().damage = GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().damage - boostPoints;
+        printDamages();
+        if (dspBoosted)
+        {
+            DPSBoostedSprite.GetComponent<RectTransform>().position = new Vector2(110, DPSBoostedSprite.GetComponent<RectTransform>().position.y);
+        }
+        DamagesBoostedSprite.SetActive(false);
+    }
 
     IEnumerator BoostDPS(float boostMultiplicator, float boostTime)
     {
-        GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().startTimeBtwShots /= boostMultiplicator;
+        float tmpStartTimeBtwShots = GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().startTimeBtwShots;
+        GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().startTimeBtwShots = tmpStartTimeBtwShots / boostMultiplicator;
+        printAmmo();
+        if (damagesBoosted)
+        {
+            DPSBoostedSprite.GetComponent<RectTransform>().position = new Vector2(135, DPSBoostedSprite.GetComponent<RectTransform>().position.y);
+        }
+        else
+        {
+            DPSBoostedSprite.GetComponent<RectTransform>().position = new Vector2(110, DPSBoostedSprite.GetComponent<RectTransform>().position.y);
+        }
+        DPSBoostedSprite.SetActive(true);
         yield return new WaitForSeconds(boostTime);
-        GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().startTimeBtwShots *= boostMultiplicator;
+        GameObject.FindGameObjectsWithTag("Weapon")[0].GetComponent<Weapon>().startTimeBtwShots = tmpStartTimeBtwShots;
+        printAmmo();
+        if (damagesBoosted)
+        {
+            DamagesBoostedSprite.GetComponent<RectTransform>().position = new Vector2(110, DamagesBoostedSprite.GetComponent<RectTransform>().position.y);
+        }
+        DPSBoostedSprite.SetActive(false);
     }
 }
