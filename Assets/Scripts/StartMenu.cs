fd6890738cde18f95b10fdbfa1640c7e3721bb26
rbwebdev <brasier.romain@gmail.com>
2019-07-12 08:11:46 +0200

    UI

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -7,9 +7,26 @@ public class StartMenu : MonoBehaviour
 {
     public GameObject startMenuUI;
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown("enter") || Input.GetKeyDown("return"))
+        {
+            PlayGame();
+        }
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("CreditsScreen");
     }
 
     public void QuitGame()
